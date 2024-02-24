b0f5b39b3b0055fa62c38f4f692de294151c221c
Léo Vetter <vetter.leo@gmail.com>
2024-02-24 08:24:16 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.get = void 0;
+function get(url) {
+    console.log("get function in vet-angular is working");
+}
+exports.get = get;
